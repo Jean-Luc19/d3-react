15e33604095c174b449efd3b2b2c0f9c197afd1f
Aaron Gottlieb <aticus19@gmail.com>
2017-04-15 13:20:25 -0600

    initial setup

---
+++ src/components/axis.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Axis extends Component {
+    componentDidMount() { this.renderAxis() }
+    componentDidUpdate() { this.renderAxis() }
+
+    renderAxis() {
+        const scale = d3.scaleLinear()
+                        .domain([0, 10])
+                        .range([0, 200])
+        const axis = d3.axisBottom(scale)
+
+        d3.select(this.refs.g)
+            .call(axis)
+    }
+
+    render() {
+        return <g transform='translate(10, 30)' ref='g' />
+    }
+}
+
+export default Axis

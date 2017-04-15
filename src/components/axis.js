d83dd0547615a3fecfc3c708d01e7f4a9830da18
Aaron Gottlieb <aticus19@gmail.com>
2017-04-15 13:47:18 -0600

    adds d3 and lodash dependencies

--- src/components/axis.js
+++ src/components/axis.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import * as d3 from "d3";
+import _ from 'lodash';
+
 class Axis extends Component {
     componentDidMount() { this.renderAxis() }
     componentDidUpdate() { this.renderAxis() }
